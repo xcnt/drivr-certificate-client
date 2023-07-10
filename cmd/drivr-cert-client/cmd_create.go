513f38a6904c042dac6ea9c057abf68f0c435c3b
Markus Herpich <markus@xcnt.io>
2023-07-11 00:29:44 +0200

    feat: make key generation configureable

--- cmd/drivr-cert-client/cmd_create.go
+++ cmd/drivr-cert-client/cmd_create.go
@@ -5,6 +5,21 @@ import (
 	"github.com/xcnt/drivr-certificate-client/cert"
 )
 
+var (
+	keyBitsFlag = &cli.IntFlag{
+		Name:    "key-bits",
+		Aliases: []string{"b"},
+		Usage:   "Number of bits for the key",
+		Value:   2048,
+	}
+	keyOutfileFlag = &cli.StringFlag{
+		Name:    "key-outfile",
+		Aliases: []string{"k"},
+		Usage:   "Output file for the key",
+		Value:   "key.pem",
+	}
+)
+
 func createCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "create",
@@ -21,11 +36,15 @@ func keyPairCommand() *cli.Command {
 		Name:   "keypair",
 		Usage:  "Create a new key pair",
 		Action: createKeyPair,
+		Flags: []cli.Flag{
+			keyBitsFlag,
+			keyOutfileFlag,
+		},
 	}
 }
 
 func createKeyPair(ctx *cli.Context) error {
-	return cert.GenerateRSAKeyPair(2048, "key.pem")
+	return cert.GenerateRSAKeyPair(ctx.Int(keyBitsFlag.Name), ctx.String(keyOutfileFlag.Name))
 }
 
 func certificateCommand() *cli.Command {
