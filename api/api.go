823f8b022954b41c38a8ac8806f404cb5a6616c3
Markus Herpich <markus@xcnt.io>
2023-09-08 08:31:21 +0200

    refac: oauth2 only if token

--- api/api.go
+++ api/api.go
@@ -2,16 +2,21 @@ package api
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/shurcooL/graphql"
 	"golang.org/x/oauth2"
 )
 
 func NewClient(apiURL, apiToken string) (*graphql.Client, error) {
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: apiToken},
-	)
-	httpClient := oauth2.NewClient(context.Background(), src)
+	var httpClient *http.Client
+
+	if apiToken != "" {
+		src := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: apiToken},
+		)
+		httpClient = oauth2.NewClient(context.Background(), src)
+	}
 
 	client := graphql.NewClient(apiURL, httpClient)
 	return client, nil
