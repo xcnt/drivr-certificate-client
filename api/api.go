b6ee2ba7bbe529df3327060ac8ee2c1da07c1776
Markus Herpich <markus@xcnt.io>
2024-04-04 09:52:59 +0200

    feat: remove oauth flow
    Since there is no convenient way to provide application consumer
    credentials and embedding them is not an option oauth flow is removed in
    favor of using api tokens the user must create.

--- api/api.go
+++ api/api.go
@@ -46,14 +46,6 @@ func injectLoggingTransport(c *http.Client) {
 func newClient(apiURL url.URL, apiToken string) (*graphql.Client, error) {
 	var httpClient *http.Client
 
-	if apiToken == "" {
-		clientID, clientSecret, err := getOAuthCredentials()
-		if err != nil {
-			return nil, err
-		}
-		apiToken = oauthFlow(apiURL, clientID, clientSecret)
-	}
-
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: apiToken},
 	)
